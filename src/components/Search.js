0cd66c628d52656f16b0c0a65138b7124949e1b7
Lewis Smith <lewislbssmith212@gmail.com>
2023-04-04 19:09:28 +0100

    Search component and tests added

---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from "react";
+import "../styles/search.css";
+
+const Search = () => {
+  return (
+    <>
+    <input className="search-input" type="text" />
+   </>)
+}
+
+export default Search
