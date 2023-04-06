77e9ebb79c77bb93ecad9b8fcc32db6d4063b675
Lewis Smith <lewislbssmith212@gmail.com>
2023-04-06 16:45:40 +0100

    get images from array function

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,22 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from "react";
 import "../styles/search.css";
+import "../requests/getImages";
+import getImages from "../requests/getImages";
 
 const Search = () => {
 
-    const { value, setValue} = useState();
+    const [ value, setValue ] = useState();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(event);
+        getImages(value);
+    }
 
   return (
     <>
-    <form className="search-form">
+    <form className="search-form" onSubmit={handleSubmit}>
         <input 
             className="search-input"
             onChange={(e) => setValue(e.target.value)}
